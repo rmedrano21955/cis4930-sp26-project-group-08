c0083063a180e5a17fccf1a9e99c549e9e495100
AJ <alejandramcletchie@gmail.com>
2026-03-27 14:29:38 -0400

    Delete data/processed/Processed_Data.py

--- data/processed/Processed_Data.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-file_path = "data/raw/sample.csv"
-
-df = pd.read_csv(file_path)
-
-df.drop(['key', 'mode', 'speechiness', 'acousticness', 'instrumentalness', 'time_signature'], axis=1, inplace=True)
-
-df.to_csv("data/processed/processed_sample.csv", index=False)
