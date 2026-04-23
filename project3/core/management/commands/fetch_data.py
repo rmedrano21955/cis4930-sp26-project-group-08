df1e3c17e736172557f6e3f8c768ef97c542a0ff
rmedrano21955 <medranoraul21955@gmail.com>
2026-04-23 19:40:45 -0400

    Add files via upload

--- project3/core/management/commands/fetch_data.py
+++ project3/core/management/commands/fetch_data.py
@@ -2,31 +2,36 @@ from django.core.management.base import BaseCommand
 from core.services.imdb_api import fetch_movies
 from core.models import Movie
 import requests
+import os
+from django.conf import settings
 
 class Command(BaseCommand):
     help = "Fetch movies from API"
 
     def handle(self, *args, **kwargs):
         
+        api_key = settings.API_KEY
+
+        if not api_key:
+            self.stderr.write("Missing API_KEY in environment")
+            return
+
         url = "http://www.omdbapi.com/"
-        api_key = "5589e759"
 
         try:
-            for page in range(1, 4):
-                response = requests.get(
-                    url,
-                    params={
-                        "apikey": api_key,
-                        "s": "john wick",
-                        "page": page
-                    },
-                    timeout=10
-                )
+            response = requests.get(
+                url,
+                params={
+                    "apikey": api_key,
+                    "s": "batman"
+                },
+                timeout=10
+            )
 
-                response.raise_for_status()
-                data = response.json()
+            response.raise_for_status()
+            data = response.json()
 
-                self.stdout.write(f"Page {page} fetched successfully")
+            self.stdout.write(str(data))
 
         except requests.exceptions.RequestException as e:
             self.stderr.write(str(e))
